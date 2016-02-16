efb7f45d1e02eb14d48601610001be799a2d029a
HIRAYAMA Ayano <a.hirayama@newdaysys.co.jp>
2016-02-16 10:35:05 +0900

    try map

--- lib/Weather/Web/Controller/Tornado.pm
+++ lib/Weather/Web/Controller/Tornado.pm
@@ -42,9 +42,70 @@ sub index :Path :Args(0) {
 	);
 
     my @tornado = $csv->tornado(\@where);
+
+#    my $mapdata;
+ #   for (@tornado){
+#	my ($p,$b) = split(" ",$_->{position});
+#	$p =~ s/(^.+)県$/$1/g;
+#	push( @{$mapdata->{$p}}, $_ ); 
+ #   }
+  #  my $map;
+   # for (keys %$mapdata){
+#	push (@$map, [ $_, $#{$mapdata->{$_}} + 1 ]);
+ #   } 
+    my $map = [
+	['北海道', 5],
+	['青森', 12],
+	['岩手', 15],
+	['宮城', 8],
+	['秋田', 3],
+	['山形', 18],
+	['福島', 22],
+	['茨城', 67],
+	['栃木', 32],
+	['群馬', 17],
+	['埼玉', 67],
+	['千葉', 56],
+	['東京', 50],
+	['神奈川', 49],
+	['新潟', 89],
+	['富山', 92],
+	['石川', 93],
+	['福井', 90],
+	['山梨', 95],
+	['長野', 70],
+	['岐阜', 73],
+	['静岡', 50],
+	['愛知', 78],
+	['三重', 74],
+	['滋賀', 76],
+	['京都', 59],
+	['大阪', 60],
+	['兵庫', 49],
+	['奈良', 99],
+	['和歌山', 90],
+	['鳥取', 63],
+	['島根', 62],
+	['岡山', 61],
+	['広島', 79],
+	['山口', 60],
+	['徳島', 64],
+	['香川', 64],
+	['愛媛', 64],
+	['高知', 62],
+	['福岡', 71],
+	['佐賀', 63],
+	['長崎', 63],
+	['熊本', 53],
+	['大分', 53],
+	['宮崎', 53],
+	['鹿児島', 54],
+	['沖縄', 53]
+	];
     my $year_list  = ['',1950..2016];
     my $month_list = ['',1..12];
     my $day_list   = ['',1..31];
+    $c->stash->{map} = $map;
     $c->stash->{year_list}  = $year_list;
     $c->stash->{month_list} = $month_list;
     $c->stash->{day_list}   = $day_list;
@@ -56,19 +117,19 @@ sub index :Path :Args(0) {
 
 sub detail :Local {
     my ( $self, $c ) = @_;
-	my $params  = $c->req->params;
-	my @date = split(' ',$params->{date});
-	my ($y,$m,$d) = split('/',$date[0]);
-	
-	$date[1] =~ s/(^.+)時頃$/$1/g;
-	$date[1] =~ s/(^.+)頃$/$1/g;
-	my $hour = $date[1] =~ /^(%d{2})$/ ? $1 : (split(":",$date[1]))[0];
-	my $min  = $date[1] =~ /^(%d{2})$/ ? '' : (split(":",$date[1]))[1];
+    my $params  = $c->req->params;
+    my @date = split(' ',$params->{date});
+    my ($y,$m,$d) = split('/',$date[0]);
+    
+    $date[1] =~ s/(^.+)時頃$/$1/g;
+    $date[1] =~ s/(^.+)頃$/$1/g;
+    my $hour = $date[1] =~ /^(%d{2})$/ ? $1 : (split(":",$date[1]))[0];
+    my $min  = $date[1] =~ /^(%d{2})$/ ? '' : (split(":",$date[1]))[1];
 
-	my ($p,$b) = split(" ",$params->{position});
-	$p =~ s/(^.+)県$/$1/g;
-	$b =~ s/(^.+)市$/$1/g;
-	my ($type,$key) = $c->gettype($y,$m,$d);
+    my ($p,$b) = split(" ",$params->{position});
+    $p =~ s/(^.+)県$/$1/g;
+    $b =~ s/(^.+)市$/$1/g;
+    my ($type,$key) = $c->gettype($y,$m,$d);
 
     my ($show,$list);
     my @prec   = $c->pdata($p);

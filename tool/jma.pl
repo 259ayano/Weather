a2bde9136457daa9e4e3ae20e9ad744efdd3ff9c
HIRAYAMA Ayano <a.hirayama@newdaysys.co.jp>
2016-03-20 15:59:28 +0900

    more easy

--- tool/jma.pl
+++ tool/jma.pl
@@ -11,6 +11,7 @@ my $ua  = LWP::UserAgent->new;
 my $dir = './tsv';
 my @precs;
 
+`rm ./tsv/*`;
 
 # 県のデータを取得
 
@@ -48,3 +49,43 @@ for my $p (@precs){
 		close $fh;
 	}
 }
+
+
+# 竜巻のデータを取得
+my $url = 'http://www.data.jma.go.jp/obd/stats/data/bosai/tornado';
+
+# 年代別の事例(url1)を取得
+
+my @years = qw /1961 1971 1981 1991 2001 2011/;
+open my $fh, '>>', "$dir/tornado" or die $!;
+for my $y (@years){ 
+    my $url1 = "$url/list/${y}.html";
+    my $res = $ua->get($url1);
+    my $con = $res->decoded_content;
+    my $tree = HTML::TreeBuilder->new;
+    $tree->parse($con);
+
+    for my $tr ($tree->look_down( _tag => 'tr')) {
+	my $line = [ map { $_->as_text !~ /詳細/ ? $_->as_text : ()} $tr->find('td') ];
+	next if ($#$line != 10);
+	say $fh join(',', @$line);
+    }
+}
+close $fh;
+
+# 最近の事例(url2)を取得
+my $url2 = "$url/new/list_new.html";
+
+my $res2 = $ua->get($url2);
+my $con2 = $res2->decoded_content;
+my $tree2 = HTML::TreeBuilder->new;
+$tree2->parse($con2);
+open my $fh, '>>', "$dir/tornado2" or die $!;
+for my $tr ($tree2->look_down('class','data2_s')->find('tr')) {
+    my $line;
+    push @$line, $_->as_text for ($tr->find('td'));
+    next if ($#$line != 13);
+    say $fh join(',', @$line);
+}
+close $fh;
+
